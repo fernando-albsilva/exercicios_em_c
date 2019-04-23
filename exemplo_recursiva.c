04dbbb802eb910434974750fa477c864dde7a30e
fernando-albsilva <fernando.albsilva@gmail.com>
2019-04-23 17:17:56 -0300

    segundo commit

--- exemplo_recursiva.c
+++ exemplo_recursiva.c
@@ -1,24 +1,48 @@
+/* Calcula o produto de um vetor de n elementos inteiros recursivamente */
+
 #include <stdio.h>
 #include <stdlib.h>
 
+void tamanho_vetor(int *);
+int *alloc_vetor(int *, int);
+int *preenche_vetor(int *, int);
 int produto(int *v, int);
 
 int main()
 {
     int n, i, *v, total;
 
-    printf("\n Digite o tamanoho do vetor\n");
-    scanf("%d",&n);
-
-    
-    v=(int*)malloc(n*sizeof(int));
-
-    for (i = 0; i < n; i++)    
-     v[i] = i + 1;
+    tamanho_vetor(&n);
+    v = alloc_vetor(v, n);
+    v = preenche_vetor(v, n);
 
     total = produto(v, n);
-    printf("\n total = %d", total);
+    printf("\n----------------------\n");
+    printf("\n total = %d\n", total);
+    printf("\n----------------------\n");
     return 0;
+}
+void tamanho_vetor(int *p_n)
+{
+
+    printf("\n Digite o tamanoho do vetor:\n");
+    scanf("%d", &*p_n);
+}
+int *alloc_vetor(int *v, int n)
+{
+    v = (int *)malloc(n * sizeof(int));
+    return v;
+}
+int *preenche_vetor(int *v, int n)
+{
+    int i = 0;
+    for (; i < n; i++)
+    {
+        printf("\n Digite v[%d] : ", i);
+        scanf("%d", &v[i]);
+    }
+
+    return v;
 }
 
 int produto(int *v, int n)
