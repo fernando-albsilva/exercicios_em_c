20776a9778bc6095307088b167a82237f10bde38
fernando-albsilva <fernando.albsilva@gmail.com>
2019-04-23 17:04:47 -0300

    segundo commit

--- fatorial_recursiva.c
+++ fatorial_recursiva.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 
 int fatorial(int);
+void chama_fat();
 
 int main()
 {
-    int n;
 
-    printf(" Digite o numero para saber seu fatorial\n");
-    scanf("%d",&n);
-
-    n=fatorial(n);
-
-    printf(" Total = %d", n);
+    chama_fat();
 
     return 0;
 }
+void chama_fat()
+{
+    int n, teste = 1;
 
+    do
+    {
+        printf("\n Digite o numero a ser calculado seu fatorial : ");
+        scanf("%d", &n);
+        printf("\n------------------\n");
+        printf("\n %d!  = %d\n", n, fatorial(n));
+        printf("\n------------------\n");
+        printf(" \nDigite qualquer numero para continuar calculando outro numero ou 0 para sair\n");
+        scanf("%d", &teste);
+    } while (teste != 0);
+}
 int fatorial(int n)
 {
-    if (n==0)
+    if (n == 0)
     {
         return 1;
     }
     else
     {
-    return (n*fatorial(n-1));
-
+        return (n * fatorial(n - 1));
     }
 }
